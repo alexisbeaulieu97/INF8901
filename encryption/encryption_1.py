f03987e12ae2ebab3465302d6a314623c45615bd
Alexis Beaulieu <alexisbeaulieu97@gmail.com>
2021-11-09 00:00:40 -0500

    Added print, added comments, changed variable name

--- encryption/encryption_1.py
+++ encryption/encryption_1.py
@@ -4,27 +4,29 @@ for i in range(ord('a'), ord('z') + 1):
     alphabet.append(chr(i))
 
 def caesar_encrypt(key: int, data: str):
-    new_data = ""
+    result = ""
     for element in data:
         if element == " ":
-            new_data += " "
+            result += " "
         elif element.isupper():
-            new_data += alphabet[(ord(element.lower()) + key - ord('a')) % len(alphabet)].upper()
+            result += alphabet[(ord(element.lower()) + key - ord('a')) % len(alphabet)].upper()
         else:
-            new_data += alphabet[(ord(element) + key - ord('a')) % len(alphabet)]
-    print(new_data)
+            result += alphabet[(ord(element) + key - ord('a')) % len(alphabet)]
+
 
 def caesar_decrypt(key: int, data: str):
-    new_data = ""
+    result = ""
     for element in data:
         if element == " ":
-            new_data += " "
+            result += " "
         elif element.isupper():
-            new_data += alphabet[(ord(element.lower()) - key - ord('a')) % len(alphabet)].upper()
+            result += alphabet[(ord(element.lower()) - key - ord('a')) % len(alphabet)].upper()
         else:
-            new_data += alphabet[(ord(element) - key - ord('a')) % len(alphabet)]
-    print(new_data)
+            result += alphabet[(ord(element) - key - ord('a')) % len(alphabet)]
 
 
-caesar_encrypt(1, "Ceci est un test") # Dfdj ftu vo uftu
-caesar_decrypt(1, "Dfdj ftu vo uftu") # Ceci est un test
+encrypted = caesar_encrypt(1, "Ceci est un test")
+decrypted = caesar_decrypt(1, "Dfdj ftu vo uftu")
+
+print(encrypted)  # Dfdj ftu vo uftu
+print(decrypted)  # Ceci est un test
